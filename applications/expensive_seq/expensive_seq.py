29eb2b9f0dd0d2c5c5b02a7585369c3058052453
Schrese <schreseholloway@gmail.com>
2020-08-04 17:14:11 -0400

    finished expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,25 @@
 # Your code here
 
-
+cache = {}
 def expensive_seq(x, y, z):
     # Your code here
-
+    # x/y/x f-string will be the key
+    entry = (x, y, z)
+    # if the entry is not in the cache 
+    if entry not in cache:
+        # if x equals 0, then return y+z (instructions say that x will not be less than 0)
+        if x <= 0:
+            v = y + z
+            cache[entry] = v
+        # otherwise perform the recursive function
+        else:
+            v = expensive_seq(x-1, y+1, z) + expensive_seq(x-2, y+2, z*2) + expensive_seq(x-3, y+3, z*3)
+            cache[entry] = v
+        # Store the cache entry
+        # Return result
+        return cache[entry]
+    # Return result
+    return cache[entry]
 
 
 if __name__ == "__main__":
