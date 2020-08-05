81724b20614bd66e1c7c4242ffa301a4bbc8544a
Schrese <schreseholloway@gmail.com>
2020-08-05 17:47:44 -0400

    re-did lookup_table using my hashtable

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,6 +1,7 @@
 # Your code here
 import math
 import random
+from hashtable import HashTableEntry, HashTable
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -32,23 +33,37 @@ def slowfun_too_slow(x, y):
 
 # USING AN F-STRING 
 # Create a cache
-cache = {}
-def slowfun(x, y):
-    """
-    Rewrite slowfun_too_slow() in here so that the program produces the same
-    output, but completes quickly instead of taking ages to run.
-    """
-    # Your code here
-    entry = f"{x},{y}"
-    # If x/y isn't in the cache, then run the slow function and create a new entry
-    if entry not in cache: 
-        v = slowfun_too_slow(x, y)
-        cache[entry] = v
-    # Store x/y as key and output of above function as value
-        return cache[entry]
-    # Otherwise just return the value from the cache     
-    return cache[entry]
+# cache = {}
+# def slowfun(x, y):
+#     """
+#     Rewrite slowfun_too_slow() in here so that the program produces the same
+#     output, but completes quickly instead of taking ages to run.
+#     """
+#     # Your code here
+#     entry = f"{x},{y}"
+#     # If x/y isn't in the cache, then run the slow function and create a new entry
+#     if entry not in cache: 
+#         v = slowfun_too_slow(x, y)
+#         cache[entry] = v
+#     # Store x/y as key and output of above function as value
+#         return cache[entry]
+#     # Otherwise just return the value from the cache     
+#     return cache[entry]
 
+
+# USING MY HASHTABLE
+ht = HashTable(8)
+
+def slowfun(x, y):
+    # entry = f"{x}, {y}"
+    entry = (x, y)
+    # print(entry, ';aoijsdf;oijao;sdfjio;asdijfo')
+
+    if ht.get(entry) is None:
+        v = slowfun_too_slow(x, y)
+        ht.put(entry, v)
+        return ht.get(entry)
+    return ht.get(entry)
 
 
 # Do not modify below this line!
